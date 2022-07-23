47867a1188e30d85c7150afceb2329219b5ae6ca
TerryW305 <terrywelch30625@gmail.com>
2022-07-23 14:20:32 +0000

    database

--- src/models.py
+++ src/models.py
@@ -15,16 +15,6 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
     def to_dict(self):
         return {}
