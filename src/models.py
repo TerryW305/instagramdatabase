dbca07d5fd749c2efde3aee1663251a9e1072660
TerryW305 <terrywelch30625@gmail.com>
2022-07-23 15:54:57 +0000

    instagramdatabase

--- src/models.py
+++ src/models.py
@@ -8,50 +8,20 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Character(Base):
-    __tablename__ = 'character' 
+class Users(Base)
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    username = Column(String(30), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    firs_tname = Column(String(30), unique=True, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String(16), unique=True, nullable=False)
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    climate = Column(String)
-
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(30), unique=True)
-    password = Column(String(16))
-    email = Column(String, unique=True)
-    firstname = Column(String)
-    lastname = Column(String)
-
-class Favorite(Base):
-     __tablename__ = 'favorite'
-     id = Column(Integer, primary_key=True)
-     character_id = relationship(Character)
-     character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
-     planet = relationship(Planet)
-     planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
-     user = relationship(User)
-     user_id = Column(Integer, ForeignKey('user.id'))
-
-     def validate():
-        if not self.planet_id and not self.character_id:
-            return False
-        return True
+class Followers(Base):
+    __tablename__ = 'followers' 
+    user = relationship('User')
+    use_from_id = Column(Integer, ForeignKey('User_id), nullable=True)
+    use_to_id = Column(Integer, ForeignKey('User_id), nullable=True)
 
 
-
-
-
-
-
-
-    #name = Column(String(250), nullable=False)
-
-
-# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
