901c933fb1102576e4b0976a38a0c1ac2e6d91d7
TerryW305 <terrywelch30625@gmail.com>
2022-07-23 15:18:36 +0000

    database

--- src/models.py
+++ src/models.py
@@ -8,16 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character' 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    climate = Column(String)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), unique=True)
+    password = Column(String(16))
+    email = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+
+class Favorite(Base):
+     __tablename__ = 'favorite'
+     id = Column(Integer, primary_key=True)
+     character_id = relationship(Character)
+     character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+     planet = relationship(Planet)
+     planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+     user = relationship(User)
+     user_id = Column(Integer, ForeignKey('user.id'))
+
+     def validate():
+        if not self.planet_id and not self.character_id:
+            return False
+        return True
 
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
+
+
+
+
+
+    #name = Column(String(250), nullable=False)
+
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
